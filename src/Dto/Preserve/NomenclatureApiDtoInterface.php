73eb8eba0dfc55d9f8ad04079a9928ffe80bef3f
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-02 02:16:45 +0300

    fixed namespaces

--- src/Dto/Preserve/NomenclatureApiDtoInterface.php
+++ src/Dto/Preserve/NomenclatureApiDtoInterface.php
@@ -18,9 +18,9 @@ use Evrinoma\DtoCommon\ValueObject\Mutable\DescriptionInterface;
 use Evrinoma\DtoCommon\ValueObject\Mutable\IdInterface;
 use Evrinoma\DtoCommon\ValueObject\Mutable\PositionInterface;
 use Evrinoma\DtoCommon\ValueObject\Mutable\TitleInterface;
-use Evrinoma\NomenclatureBundle\DtoCommon\ValueObject\Mutable\ItemInterface;
-use Evrinoma\NomenclatureBundle\DtoCommon\ValueObject\Mutable\ItemsInterface;
+use Evrinoma\NomenclatureBundle\DtoCommon\ValueObject\Mutable\ItemApiDtoInterface;
+use Evrinoma\NomenclatureBundle\DtoCommon\ValueObject\Mutable\ItemsApiDtoInterface;
 
-interface NomenclatureApiDtoInterface extends IdInterface, TitleInterface, PositionInterface, ActiveInterface, DescriptionInterface, ItemInterface, ItemsInterface
+interface NomenclatureApiDtoInterface extends IdInterface, TitleInterface, PositionInterface, ActiveInterface, DescriptionInterface, ItemApiDtoInterface, ItemsApiDtoInterface
 {
 }
