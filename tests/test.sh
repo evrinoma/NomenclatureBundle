385cc6762a92fe5abb18579ef5011d5c2b5a09c9
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 20:53:52 +0300

    fixed code style v8

--- tests/test.sh
+++ tests/test.sh
@@ -7,5 +7,7 @@ rm -rf vendor
 rm -rf composer.lock
 composer install --dev
 rm -rf /tmp/NomenclatureBundle
+
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
-echo
+status=$(cat /tmp/test.dump | grep "ERRORS!")
+[ -z "$status" ] && return 0 ||  exit -1
